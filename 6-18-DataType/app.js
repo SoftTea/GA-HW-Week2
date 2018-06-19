2823be9cd8ed7bd542485d825f19f0d2e4c8a4f2
Tim Mickle <TimothyRMickle@gmail.com>
2018-06-18 19:42:28 -0500

    1st commit -selecting datatypes

---
+++ 6-18-DataType/app.js
@@ -0,0 +1,44 @@
+// Datatypes
+// Refresher
+
+// For each of the following, write which datatypes you would use to represent the data, and then give a small example of the data structure:
+
+// A light switch that can be either on or off.
+// Booleans true / false
+
+// A user's email address. 
+// string `this is a string`, 'so is this', "and this as well"
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// An Object {
+//     hull: 'type of hull or any data type',
+//     laserBlasters(){
+//         we could fire our blasters
+//     }
+// }
+
+// A list of student names from our class.
+// Array, such as['Jill', 'Jane', 'Bill', 'etc']
+
+// A list of student names from our class, each with a location.
+// Array with nested objects;
+// [{
+//     name: 'Jill',
+//     location: 'austin'
+// }, {
+//     name: 'Bill',
+//     location: 'Austin'
+// }]
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.An Array with nested object which also hold an array within the objects.
+
+// [{
+//     name: 'Jill',
+//     location: 'austin',
+//     favTv: ['Whatever this is', 'Another show']
+// }, {
+//     name: 'Bill',
+//     location: 'Austin',
+//     favTv: ['tvtvtv']
+// }]
+
