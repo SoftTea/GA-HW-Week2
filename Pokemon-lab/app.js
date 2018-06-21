179ad75a0d34288acd95d2cbbbda15ea083cc750
Tim Mickle <TimothyRMickle@gmail.com>
2018-06-20 23:47:12 -0500

    Expanded newHand(), added card selection playerFromHand into game state, added resolution functions for the game state.

--- Pokemon-lab/app.js
+++ Pokemon-lab/app.js
@@ -59,6 +59,8 @@ const player = {
     score: 0,
     cardsInDeck: [],
     cardsInHand: [],
+    cardPicked: {},
+    discard: [],
     cardDealerTracker: true
 }
 const computer = {
@@ -66,6 +68,8 @@ const computer = {
     score: 0,
     cardsInDeck: [],
     cardsInHand: [],
+    cardPicked: {},
+    discard: [],
     cardDealerTracker: false
 }
 const dealDecks = () => {
@@ -94,22 +98,83 @@ const dealDecks = () => {
     }
 }
 
-dealDecks();
+
 
 const newHands = () => {
+    if(player.cardsInHandNumber >0) {
+
+        player.cardsInHand.forEach((curr)=>{
+            player.discard.push(curr)
+        })
+        player.cardsInHand = [];
+        player.discard.push(player.cardPicked);
+        player.cardPicked = {};
+        player.cardsInHandNumber = 0;
+        
+        computer.cardsInHand.forEach((curr)=>{
+            computer.discard.push(curr)
+        })
+        computer.cardsInHand =[];
+        computer.discard.push(player.cardPicked);
+        computer.cardPicked = {};
+        computer.cardsInHandNumber = 0;
+    }
     for (let i = 0; i < 3; i++) {
+        // Catching to make sure we don't draw infitently (Edge Case)
+        if(player.cardsInDeck.length === 0) {
+            return console.log("The Game is Over")
+        }
         const randomNumberP = Math.floor(Math.random() * ((player.cardsInDeck.length - 1) + 1));
         const randomNumberC = Math.floor(Math.random() * ((computer.cardsInDeck.length - 1) + 1));
 
         player.cardsInHand.push(player.cardsInDeck[randomNumberP]);
+        player.cardsInHandNumber++;
 
         player.cardsInDeck = player.cardsInDeck.filter(ele => ele.name !== player.cardsInDeck[randomNumberP].name)
 
         computer.cardsInHand.push(computer.cardsInDeck[randomNumberP]);
+        computer.cardsInHandNumber++;
 
         computer.cardsInDeck = computer.cardsInDeck.filter(ele => ele.name !== computer.cardsInDeck[randomNumberP].name)
     }
 
 }
 
+
+// player.cardsInHand[i]
+const playFromHand = (indexNum)=>{
+    player.cardPicked = player.cardsInHand[indexNum];
+    player.cardsInHand.splice(indexNum,1);
+    player.cardsInHandNumber--
+
+    const randomizer = Math.floor(Math.random()*(2+1));
+    
+    computer.cardPicked = computer.cardsInHand[randomizer];
+    computer.cardsInHand.splice(randomizer,1);
+    computer.cardsInHandNumber--;
+}
+
+const resolvePlayedCards = ()=>{
+    if(player.cardPicked.damage === computer.cardPicked.damage) {
+        console.log(`${player.cardPicked.name} and ${computer.cardPicked.name} are equal matches. It is a draw this round`);
+    } else if( player.cardPicked.damage>computer.cardPicked.damage){
+        console.log(`${player.cardPicked.name} is stronger than ${computer.cardPicked.name}, Player has won`);
+        player.score++;
+    } else {
+        console.log(`${computer.cardPicked.name} is stronger than ${player.cardPicked.name}, Computer has won`);
+        computer.score++;
+    }
+    if (player.cardsInDeck.length<3) {
+        console.log(`The game is over kiddo`)
+    }
+}
+
+
+dealDecks();
 newHands();
+playFromHand(1);
+resolvePlayedCards();
+newHands();
+
+console.log(player.cardPicked);
+console.log(computer.cardPicked);
