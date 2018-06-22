d85f141236154a90c4674fe328e84f56f8bdcfac
Tim Mickle <TimothyRMickle@gmail.com>
2018-06-21 21:17:12 -0500

    Commit 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -22,3 +22,36 @@ const $h4 = $('<h2/>');
 $container.append($('<h4/>').attr('class', 'cat').text('Sims'));
 $container.append($h4.text('Holly Wand with Unicorn Hair Core'));
 console.log($h4)
+
+const $ul = $('<ul/>');
+$ul.attr('storage', 'trunk');
+$container.append($ul);
+
+const $li = $('<li/>');
+$li.text('butter beer');
+$ul.append($li);
+
+const $li1 = $('<li/>');
+$li1.text('invisibility cloak');
+$li1.attr('class','secrective');
+$ul.append($li1);
+
+const $li2 = $('<li/>');
+$li2.text('magic map');
+$li2.attr('class','secrective');
+$ul.append($li2);
+
+const $li3 = $('<li/>');
+$li3.text('time turner');
+$li3.attr('class','secrective');
+$ul.append($li3);
+
+const $li4 = $('<li/>');
+$li4.text('leash');
+$li4.attr('class','cat');
+$ul.append($li4);
+
+const $li5 = $('<li/>');
+$li5.text('Bertie Botts Every Flavor Jelly Beans.');
+$ul.append($li5);
+
