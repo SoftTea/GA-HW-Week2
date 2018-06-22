16146fcd3621d61e5fcf7a25cf9d74d16e2b0eb1
Tim Mickle <TimothyRMickle@gmail.com>
2018-06-21 20:36:35 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -10,3 +10,15 @@ const $h1 = $('<h1></h1>');
 console.log($h1);
 $h1.text("Hogwarts");
 $container.append($h1);
+
+const $h2 = $('<h2/>');
+$h2.text('Timo');
+$container.append($h2);
+const $h3 = $('<h2/>');
+$container.append($h3);
+$h3.text('Ravenclaw')
+const $h4 = $('<h2/>');
+
+$container.append($('<h4/>').attr('class', 'cat').text('Sims'));
+$container.append($h4.text('Holly Wand with Unicorn Hair Core'));
+console.log($h4)
